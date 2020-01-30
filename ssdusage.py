fb39673c3118432910119830eb4b0a714ae0c252
grapeit <grapeit@the-grape.com>
2020-01-29 20:56:41 -0600

    init commit with ssd tool

---
+++ ssdusage.py
@@ -0,0 +1,61 @@
+import sys
+import subprocess
+import re
+
+
+args = sys.argv
+if len(args) != 2:
+    print("device name expected")
+    exit(1)
+
+
+KB = 1024
+MB = KB * 1024
+GB = MB * 1024
+TB = GB * 1024
+
+
+def getValue(val, unit):
+    v = float(val)
+    if unit == "TB":
+        v *= TB
+    elif unit == "GB":
+        v *= GB
+    elif unit == "MB":
+        v *= MB
+    elif unit == "KB":
+        v *= KB
+    return v
+
+
+def parseUsage(data):
+    read, written = 0, 0
+    for ln in data.split("\n"):
+        m = re.match(r"Data Units (?P<op>\w+): *.+\[(?P<val>[\d\.]+) (?P<unit>\w+)\]", ln)
+        if m:
+            if m.group("op") == "Read":
+                read = getValue(m.group("val"), m.group("unit"))
+            elif m.group("op") == "Written":
+                written = getValue(m.group("val"), m.group("unit"))
+    return read, written
+
+
+def formatSize(value):
+    if value > TB:
+        return "%lg TB" % (value / TB)
+    elif value > GB:
+        return "%lg GB" % (value / GB)
+    elif value > MB:
+        return "%lg MB" % (value / MB)
+    elif value > KB:
+        return "%lg KB" % (value / KB)
+    return value
+
+
+sp = subprocess.run(["smartctl", "-a", "/dev/" + args[1]], stdout=subprocess.PIPE)
+usage = parseUsage(sp.stdout.decode())
+
+
+print("Read: ", formatSize(usage[0]))
+print("Written: ", formatSize(usage[1]))
+print("Total: ", formatSize(usage[0] + usage[1]))
